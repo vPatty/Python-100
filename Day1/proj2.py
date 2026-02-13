c7ee75625fa4b358ca1cec2422a660c61a5d09fa
Patrick <pcano5@miners.utep.edu>
2026-02-13 12:10:02 -0700

    Made quick and simple length program

---
+++ Day1/proj2.py
@@ -0,0 +1,9 @@
+def get_length(a):
+    length = len(a)
+    return length
+
+print("Welcome")
+
+user_name = input("Please enter your name: \n")
+
+print(f"Your name has a total of {get_length(user_name)} characters")
